7e1006df3ac56e03737dea700cd119bdc20bf99c
ysz951 <shengyang_zhou@baylor.edu>
2020-07-23 03:38:37 -0500

    update

--- src/app.js
+++ src/app.js
@@ -4,30 +4,50 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
 app.use(cors())
+app.use(helmet())
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+        res.json(articles)
+    })
+    .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+        if (!article) {
+            return res.status(404).json({
+                error: { message: `Article doesn't exist` }
+            })
+        }
+        res.json(article)
+    })
+    .catch(next)
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
     response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
