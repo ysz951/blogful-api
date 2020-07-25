4a781c7df0feecae6f9a09d170387da96d0efc75
ysz951 <shengyang_zhou@baylor.edu>
2020-07-24 21:20:23 -0500

    update

--- src/app.js
+++ src/app.js
@@ -12,12 +12,6 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
 app.use(cors())
 app.use(helmet())
-
-app.use('/articles', articlesRouter)
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
-
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
@@ -28,5 +22,16 @@ app.use(function errorHandler(error, req, res, next) {
   }
   res.status(500).json(response)
 })
+app.use('/articles', articlesRouter)
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.get('/xss', (req, res) => {
+  res.cookie('secretToken', '1234567890');
+  res.sendFile(__dirname + '/xss-example.html');
+});
+
+
 
 module.exports = app
